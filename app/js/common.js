f095fab94740412325ca8d845428e8a21c32201f
Sviatoslav Luchyshyn <s.luchyshyn@gmail.com>
2018-04-18 20:08:29 +0300

    Animation added + mobile view fixed

--- app/js/common.js
+++ app/js/common.js
@@ -14,61 +14,29 @@ var $isAnimated1 = $('.section-interview .is-animated'),
     $isAnimated5 = $('.under-control .is-animated'),
     $isAnimated5Single = $('.under-control .is-animated__single'),
     $isAnimated6 = $('.section-technologies .is-animated'),
-    $isAnimated6Single = $('.section-technologies .is-animated__single'),
-    $isAnimated7 = $('.organization .is-animated'),
-    $isAnimated7Single = $('.organization .is-animated__single');
+    $isAnimated6Single = $('.section-technologies .is-animated__single');
 
-
+var $animation_elements = $('.trust-item');
+var $window = $(window);
 
 $(document).ready(function () {
     $('.section-baner-wrap').addClass('load');
-    $('#fullpage').fullpage({
-        //Navigation
-        // menu: '#menu',
-        lockAnchors: false,
-        // anchors: ['firstPage', 'secondPage'],
-        // navigation: true,
-        // navigationPosition: 'right',
-        // navigationTooltips: ['firstSlide', 'secondSlide'],
-        showActiveTooltip: false,
-        slidesNavigation: false,
-        slidesNavPosition: 'bottom',
 
+    $('#fullpage').fullpage({
+
+        //Navigation
+        // navigation: false,
+        navigation: false,
+        navigationPosition: 'right',
         //Scrolling
         css3: true,
         scrollingSpeed: 800,
-        // autoScrolling: true,
-        // fitToSection: true,
-        // fitToSectionDelay: 1000,
-        scrollBar: false,
         easing: 'easeInOutCubic',
         easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        // loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        dragAndMove: false,
-        offsetSections: false,
-        resetSliders: false,
-        fadingEffect: false,
         normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowReset: false,
-        scrollOverflowOptions: null,
-        touchSensitivity: 35,
-        normalScrollElementTouchThreshold: 6,
-        bigSectionsDestination: null,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
+        touchSensitivity: 22,
+        normalScrollElementTouchThreshold: 1,
         //Design
-        controlArrows: true,
         verticalCentered: true,
         sectionsColor: ['transparent', '#fff'],
         paddingTop: '0',
@@ -76,112 +44,110 @@ $(document).ready(function () {
         fixedElements: '.section-baner-wrap',
         responsiveWidth: 767,
         responsiveHeight: 660,
-        responsiveSlides: false,
-        parallax: false,
-        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-        //Custom selectors
         sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        // navigation: true,
-
-
         lazyLoading: true,
         //events
         onLeave: function (index, nextIndex, direction) {
+            $(window).resize(function () {
+                if ($("body").prop("clientWidth") > 768) {
+                    if (index == 1) {
+                        $isAnimated1.addClass('animated bounceInUp').css('animation-delay', '0.5s');
+                        $isAnimated1.eq(1).css('animation-delay', '0.5s');
+                        $isAnimated1.eq(2).css('animation-delay', '1.5s');
 
-            if (index == 1) {
-                $isAnimated1.addClass('animated bounceInUp').css('animation-delay', '1s');
-                $isAnimated1.eq(1).css('animation-delay', '0.5s');
-                $isAnimated1.eq(2).css('animation-delay', '0.5s');
+                        $isAnimated1Single.addClass('animated bounceInDown').css('animation-delay', '1.3s');
+                        $isAnimated1Single.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated1Single.eq(2).css('animation-delay', '1s');
+                    }
+                    if (index == 2) {
+                        $isAnimated2Single.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated2Single.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated2Single.eq(2).css('animation-delay', '2s');
+                        $isAnimated2Single.eq(3).css('animation-delay', '2.5s');
+                        $isAnimated2Single.eq(4).css('animation-delay', '3s');
 
-                $isAnimated1Single.addClass('animated bounceInDown').css('animation-delay', '1.3s');
-                $isAnimated1Single.eq(1).css('animation-delay', '1.5s');
-                $isAnimated1Single.eq(2).css('animation-delay', '1s');
-            }
-            if (index == 2) {
-                $isAnimated2Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated2Single.eq(1).css('animation-delay', '2s');
-                $isAnimated2Single.eq(2).css('animation-delay', '2.5s');
-                $isAnimated2Single.eq(3).css('animation-delay', '3s');
-                $isAnimated2Single.eq(4).css('animation-delay', '3.5s');
+                        $isAnimated3.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated3.eq(1).css('animation-delay', '1s');
+                        $isAnimated3.eq(2).css('animation-delay', '1s');
+                        $isAnimated3.eq(3).css('animation-delay', '1.5s');
+                        $isAnimated3.eq(4).css('animation-delay', '2s');
+                    }
+                    if (index == 3) {
+                        $isAnimated3Single.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated3Single.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated3Single.eq(2).css('animation-delay', '2s');
+                        $isAnimated3Single.eq(3).css('animation-delay', '2.5s');
+                        $isAnimated3Single.eq(4).css('animation-delay', '3s');
+                    }
+                    if (index == 4) {
 
-                $isAnimated3.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated3.eq(1).css('animation-delay', '1s');
-                $isAnimated3.eq(2).css('animation-delay', '1s');
-                $isAnimated3.eq(3).css('animation-delay', '1.5s');
-                $isAnimated3.eq(4).css('animation-delay', '2s');
-            }
-            if (index == 3) {
-                $isAnimated3Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated3Single.eq(1).css('animation-delay', '2s');
-                $isAnimated3Single.eq(2).css('animation-delay', '2.5s');
-                $isAnimated3Single.eq(3).css('animation-delay', '3s');
-                $isAnimated3Single.eq(4).css('animation-delay', '3.5s');
-            }
-            if (index == 4) {
-                console.log(index);
-                $isAnimated4Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated4Single.eq(1).css('animation-delay', '2s');
-                $isAnimated4Single.eq(2).css('animation-delay', '3s');
-                $isAnimated4Single.eq(3).css('animation-delay', '5s');
-                $isAnimated4Single.eq(4).css('animation-delay', '6s');
+                        $isAnimated4Single.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated4Single.eq(1).css('animation-delay', '2s');
+                        $isAnimated4Single.eq(2).css('animation-delay', '2.5s');
+                        $isAnimated4Single.eq(3).css('animation-delay', '3s');
+                        $isAnimated4Single.eq(4).css('animation-delay', '3.5s');
 
 
-            }
-            if (index == 5) {
-                // console.log(index);
-                $isAnimated5Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated5Single.eq(1).css('animation-delay', '2s');
-                $isAnimated5Single.eq(2).css('animation-delay', '3s');
-                $isAnimated5Single.eq(3).css('animation-delay', '5s');
-                $isAnimated5Single.eq(4).css('animation-delay', '6s');
+                    }
+                    if (index == 5) {
+                        $isAnimated5.addClass('animated bounceInUp').css('animation-delay', '1s');
+                        $isAnimated5.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated5.eq(2).css('animation-delay', '2s');
 
-            }
-            if (index == 6) {
-                // console.log(index);
-                $isAnimated6Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated6Single.eq(1).css('animation-delay', '2s');
-                $isAnimated6Single.eq(2).css('animation-delay', '2.5s');
-                $isAnimated6Single.eq(3).css('animation-delay', '3s');
-                $isAnimated6Single.eq(4).css('animation-delay', '3.5s');
-                $isAnimated6Single.eq(5).css('animation-delay', '4s');
-            }
-            if (index == 7) {
-                console.log(index);
-                $isAnimated7Single.addClass('animated bounceInDown').css('animation-delay', '1s');
-                $isAnimated7Single.eq(1).css('animation-delay', '2s');
-                $isAnimated7Single.eq(2).css('animation-delay', '3s');
-                $isAnimated7Single.eq(3).css('animation-delay', '5s');
-                $isAnimated7Single.eq(4).css('animation-delay', '6s');
+                        $isAnimated5Single.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated5Single.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated5Single.eq(2).css('animation-delay', '2s');
+                        $isAnimated5Single.eq(3).css('animation-delay', '2.5s');
+                        $isAnimated5Single.eq(4).css('animation-delay', '3s');
 
-            }
+                    }
+                    if (index == 6) {
+                        $isAnimated6Single.addClass('animated bounceInDown').css('animation-delay', '1s');
+                        $isAnimated6Single.eq(1).css('animation-delay', '1.5s');
+                        $isAnimated6Single.eq(2).css('animation-delay', '2s');
+                        $isAnimated6Single.eq(3).css('animation-delay', '2.5s');
+                        $isAnimated6Single.eq(4).css('animation-delay', '3s');
+                        $isAnimated6Single.eq(5).css('animation-delay', '3.5s');
+                    }
+                }
+            });
+            $(window).resize();
 
-        },
+        }
+        ,
         afterLoad: function (anchorLink, index) {
-        },
+        }
+        ,
         afterRender: function () {
-        },
+        }
+        ,
         afterResize: function () {
-        },
+        }
+        ,
         afterResponsive: function (isResponsive) {
-        },
+        }
+        ,
         afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-        },
+        }
+        ,
         onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
         }
     });
-    });
+});
 
 
-// animation start
-// AOS.init({
-//
-// });
+$(window).on('scroll', function () {
 
-var $animation_elements = $('.trust-item');
-var $window = $(window);
+    var interview = $('.section-interview').offset().top;
+    var windowTop = $(window).scrollTop();
+    if (windowTop + 100 > interview) {
+        $('body').addClass('show-interview');
+    } else {
+        $('body').removeClass('show-interview');
+    }
+});
+
 
 function check_if_in_view() {
     var window_height = $window.height();
@@ -207,7 +173,7 @@ var mouseX = 85,
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight * 2;
-console.log(windowHalfY);
+// console.log(windowHalfY);
 init();
 animate();
 
@@ -280,8 +246,7 @@ function onDocumentMouseMove(event) {
 function onDocumentTouchStart(event) {
 
     if (event.touches.length === 1) {
-
-        event.preventDefault();
+        // event.preventDefault();
 
         mouseX = event.touches[0].pageX - windowHalfX;
         mouseY = event.touches[0].pageY - windowHalfY;
@@ -294,7 +259,7 @@ function onDocumentTouchMove(event) {
 
     if (event.touches.length === 1) {
 
-        event.preventDefault();
+        // event.preventDefault();
 
         mouseX = event.touches[0].pageX - windowHalfX;
         mouseY = event.touches[0].pageY - windowHalfY;
